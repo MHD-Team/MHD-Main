342662ca2ca3bfa140d75d7d1185a44a012c973e
Nino6781 <nic.tolksdorf@outlook.de>
2021-10-13 11:23:15 +0200

    Zurück Button Seite 404

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jeeva\Tanmay\MHD-Backend\env
+set VIRTUAL_ENV=C:\Users\nicto\Documents\MHD-Main\env
 
 if not defined PROMPT set PROMPT=$P$G
 
